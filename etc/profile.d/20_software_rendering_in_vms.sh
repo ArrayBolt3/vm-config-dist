263bb56459a7e6a63fb361e8e14995cf22bfcbe7
Patrick Schleizer <adrelanos@whonix.org>
2026-09-06 13:21:34 -0400

    fix

--- etc/profile.d/20_software_rendering_in_vms.sh
+++ etc/profile.d/20_software_rendering_in_vms.sh
@@ -14,9 +14,6 @@
 ## wins) and only on 'software'; accelerated/unknown -> leave unset (err toward
 ## acceleration available).
 
-# shellcheck source=../../../helper-scripts/usr/libexec/helper-scripts/has.sh
-source "${HELPER_SCRIPTS_PATH:-}"/usr/libexec/helper-scripts/has.sh
-
 if ! has detect-software-rendering >/dev/null 2>/dev/null; then
   true "${0}: INFO: detect-software-rendering does not exist."
   return 0
